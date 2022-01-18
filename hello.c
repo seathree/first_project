d063b970b7b239042a49f1f3ea7d53fef65ca7cb
seathree <park.yang.ho@gmail.com>
2022-01-18 14:20:06 +0900

    main

--- hello.c
+++ hello.c
@@ -7,6 +7,12 @@ int main()
      printf("Hello World!\n");
      printf("Hello World!\n");
      printf("Hello World!\n");
+     printf("Hello World!\n");
+     printf("Hello World!\n");
+     printf("Hello World!\n");
+     printf("Hello World!\n");
+     printf("Hello World!\n");
+     printf("Hello World!\n");
      return 0; 
 }
 
